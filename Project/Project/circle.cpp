af7872c80a5ada2e3bf33f6f64f6cd41b612822b
PetrovaAnastasiia <petrovanastya_01@mail.ru>
2021-02-26 21:16:45 +0300

    Добавлен класс окружность

--- Project/Project/circle.cpp
+++ Project/Project/circle.cpp
@@ -1,11 +1,50 @@
 #include "circle.h"
+#include <iostream>
+#include <math.h>
 
+using namespace std;
 
-circle::circle()
-{
+void Circle::set_center(Point center) {
+	_center = center;
 }
 
+Point Circle::get_center() {
+	return _center;
+}
 
-circle::~circle()
-{
+void Circle::set_rad(double rad) {
+	if (rad <= 0) {
+		cout << "Error assigning a negative or zero value to the radius" << endl;
+		_rad = 1;     //при неправильном вводе радиуса ставится радиус равный 1
+	}
+	else _rad = rad;
+}
+
+double Circle::get_rad() {
+	return _rad;
+}
+
+Circle::Circle(Point center = 0, double _rad = 1) {
+	set_center(center), set_rad(_rad);
+}
+
+//Печать уравнение окружности
+void print_equation(Circle c1) {
+	Point t;
+	t = c1.get_center();
+	double xc = t.GetX();
+	double yc = t.GetY();
+	double r = c1.get_rad();
+	cout << "(x";
+	if (xc < 0) cout << "+" << abs(xc) << ")^2+(y";
+	else if (xc > 0) cout << "-" << xc << ")^2+(y";
+	if (yc < 0) cout << "+" << abs(yc) << ")^2=" << r * r;
+	else if (yc > 0) cout << "-" << yc << ")^2=" << r * r;
+}
+
+//Длина окружности (дуги)
+double length_circle(Circle c1) {
+	double r = c1.get_rad();
+	double l = 2 * 3.14 * r;
+	return l;
 }
--- Project/Project/circle.h
+++ Project/Project/circle.h
@@ -1,7 +1,20 @@
 #pragma once
-class circle
-{
+#include "point.h"
+
+class Circle {
 public:
-	circle();
-	~circle();
+	void set_center(Point center);
+	Point get_center();
+
+	double get_rad();
+	void set_rad(double _rad);
+
+	Circle(Point center, double _rad);
+private:
+	Point _center;
+	double _rad;
 };
+
+void print_equation(Circle c1);
+
+double length_circle(Circle c1);
