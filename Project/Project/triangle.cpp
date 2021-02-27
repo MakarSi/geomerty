914ec173636a7393458a2eeacd9e62f448799f93
Kutuzova <postcrossing2002@mail.ru>
2021-02-27 15:35:09 +0300

    Изменен класс треугольник

--- Project/Project/triangle.cpp
+++ Project/Project/triangle.cpp
@@ -3,49 +3,73 @@
 #include <iostream>
 using namespace std;
 
-void Triangle::Set_point1(Point p1) {
+void Triangle::set_point1(Point p1) {
 	_p1 = p1;
 }
-Point Triangle::Get_point1() {
+Point Triangle::get_point1() {
 	return _p1;
 }
 
-void Triangle::Set_point2(Point p2) {
+void Triangle::set_point2(Point p2) {
 	_p2 = p2;
 }
-Point Triangle::Get_point2() {
+Point Triangle::get_point2() {
 	return _p2;
 }
 
-void Triangle::Set_point3(Point p3) {
+void Triangle::set_point3(Point p3) {
 	_p3 = p3;
 }
-Point Triangle::Get_point3() {
+Point Triangle::get_point3() {
 	return _p3;
 }
 
-
 Triangle::Triangle(Point p1, Point p2, Point p3) {
-	Set_point1(p1); Set_point2(p2); Set_point3(p3);
+	if ((p3.get_x() - p1.get_x()) * (p2.get_y() - p1.get_y()) == (p2.get_x() - p1.get_x()) * (p3.get_y() - p1.get_y())) {
+		throw "Error: A degenerate triangle!";
+	}
+	set_point1(p1); set_point2(p2); set_point3(p3);
 }
 
-void Triangle::Print_coordinate_of_tops() {
-	_p1.PrintCoordinate();
+void Triangle::print_coordinate_of_tops() {
+	_p1.print_coordinate();
 	cout << ' ';
-	_p2.PrintCoordinate();
+	_p2.print_coordinate();
 	cout << ' ';
-	_p3.PrintCoordinate();
+	_p3.print_coordinate();
+	cout << endl;
 }
 
-double Perimeter(Triangle t1) {
-	return Distance(t1.Get_point1(), t1.Get_point2()) + Distance(t1.Get_point2(), t1.Get_point3())
-		 + Distance(t1.Get_point1(), t1.Get_point3());
+double Triangle::perimeter() {
+	return distance(_p1, _p2) + distance(_p2, _p3) + distance(_p1, _p3);
 }
 
-double Square(Triangle t1) {
-	double a = Distance(t1.Get_point1(), t1.Get_point2());
-	double b = Distance(t1.Get_point2(), t1.Get_point3());
-	double c = Distance(t1.Get_point1(), t1.Get_point3());
+double Triangle::square() {
+	double a = distance(_p1, _p2);
+	double b = distance(_p2, _p3);
+	double c = distance(_p1, _p3);
 	double p = (a + b + c) / 2;
 	return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+void Triangle::shift_right_or_left(double k) {
+	_p1.set_x(_p1.get_x() + k);
+	_p2.set_x(_p2.get_x() + k);
+	_p3.set_x(_p3.get_x() + k);
+}
+
+void Triangle::shift_up_or_down(double k) {
+	_p1.set_y(_p1.get_y() + k);
+	_p2.set_y(_p2.get_y() + k);
+	_p3.set_y(_p3.get_y() + k);
+}
+
+bool Triangle::operator== (const Triangle& t) {
+	bool f = this->_p1 == t._p1 && this->_p2 == t._p2 && this->_p3 == t._p3;
+	return f;
+}
+
+bool Triangle::operator!= (const Triangle& t) {
+	bool f = this->_p1 != t._p1 || this->_p2 != t._p2 || this->_p3 != t._p3;
+	return f;
 }
--- Project/Project/triangle.h
+++ Project/Project/triangle.h
@@ -2,19 +2,26 @@
 #include "point.h"
 class Triangle {
 public:
-	void Set_point1(Point p1);
-	Point Get_point1();
+	void set_point1(Point p1);
+	Point get_point1();
 
-	void Set_point2(Point p2);
-	Point Get_point2();
+	void set_point2(Point p2);
+	Point get_point2();
 
-	void Set_point3(Point p3);
-	Point Get_point3();
+	void set_point3(Point p3);
+	Point get_point3();
 
-	Triangle(Point p1 = 0, Point p2 = 0, Point p3 = 0); //по точкам создаем треугольник
-	void Print_coordinate_of_tops();
+	Triangle(Point p1 = 0, Point p2 = 0, Point p3 = 0);//по точкам создаем треугольник
+	void print_coordinate_of_tops();//печатает координаты вершин треугольника
+
+	double perimeter();//находит периметр треугольника
+	double square();//находит площадь треугольника
+
+	void shift_right_or_left(double k);//при k>0 сдвиг вправо, иначе влево
+	void shift_up_or_down(double k);//при k>0 сдвиг вверх, иначе вниз
+
+	bool operator== (const Triangle& t);//Перегрузка оператора равенства
+	bool operator!= (const Triangle& t);//Перегрузка оператора неравнества
 private:
 	Point _p1, _p2, _p3;
 };
-double Perimeter(Triangle t1);
-double Square(Triangle t1);
