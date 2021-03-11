b636b00145bb78a0a7a6da739483e52af72a73b6
PetrovaAnastasiia <petrovanastya_01@mail.ru>
2021-03-11 13:49:17 +0300

    Изменено меню - теперь со стрелочками

--- Project/Project/menu.cpp
+++ Project/Project/menu.cpp
@@ -2,48 +2,75 @@
 #include <Windows.h>
 #include <conio.h>
 #include <algorithm>
+#include <string>
+#include "point.h"
+#include "circle.h"
+#include "triangle.h"
+#include "line.h"
+#include "polygon.h"
+#include "segment.h"
+#include "ring.h"
 
 using namespace std;
 
-/*Цветовое решение*/
-void set_color(int sctext, int scfon) {
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(h, (scfon << 4) + sctext);
-}
+const int N = 8; // Количество элементов в меню
 
-/*Печать основного меню*/
-int print_menu_key() {
-	set_color(15, 5);
-	cout << "1.Действия с прямой" << endl;
-	cout << "2.Действия с окружностью" << endl;
-	cout << "3.Действия с треугольником" << endl;
-	cout << "4.Действия с многоугольником" << endl;
-	cout << "5.Выход" << endl;
-	set_color(15, 10);
-	cout << "Номер в меню:" << endl;
-	int item;
-	cin >> item;
-	return item;
+int print_menu(string* a) {
+	int key = 0, move;
+	while (true) {
+		cout << "Выберите пункт меню:\n\n";
+		cout << "Введите ESC, чтобы выйти\n";
+
+		for (int i = 0; i < N; i++) {
+			if (i != key) cout << i + 1 << ". " << a[i] << endl;
+			else cout << "\x1B[34m" << i + 1 << ". " << a[i] << "\033[0m\n";
+		}
+
+		cout << '\n';
+		move = _getch();
+
+		if (move == 27) {
+			system("cls");
+			return N + 1;
+		}
+
+		if (move == 224) move = _getch();
+
+		if (move == 13) {
+			system("cls");
+			return key + 1;
+		}
+		else if (move == 72) key = ((key - 1) % N + N) % N;
+		else if (move == 80) key = (key + 1) % N;
+
+		system("cls");
+	}
+
+	return key;
 }
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	set_color(15, 15);
-	int item;
-	do {
-		item = print_menu_key();
-		switch (item)
-		{
-		case 1:; break;
-		case 2:; break;
-		case 3:; break;
-		case 4:; break;
-		case 5: return 0; break;
-		default:
-			break;
-		}
-	} while (item != 5);
-	return 0;
+	setlocale(LC_ALL, "");
+	string* A = new string[N];
+	A[0] = "Действия с прямыми";
+	A[1] = "Действия с лучом";
+	A[2] = "Действия с треугольником";
+	A[3] = "Действия с окружностью";
+	A[4] = "Действия с кругом";
+	A[5] = "Действия с многоугольником";
+	A[6] = "Действия с четырехугольником";
+	A[7] = "Действия с векторами";
+
+	while (true) {
+		int key = print_menu(A);
+		if (key == 1);
+		  else if (key == 2);
+		    else if (key == 3);
+		      else if (key == 4);
+		        else if (key == 5);
+		          else if (key == 6);
+		            else if (key == 7);
+		              else if (key == 8);
+		                else break;
+	}
 }
