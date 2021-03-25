bc494b08379861dec3a0df2efa0ff48a6ab2409b
Kutuzova <postcrossing2002@mail.ru>
2021-03-25 17:15:21 +0300

    Добавлена константность и изменены некоторые функции в классе вектор

--- Project/Project/vector.cpp
+++ Project/Project/vector.cpp
@@ -1,36 +1,31 @@
 #include "vector.h"
-void Vector::set_points(Point &p1, Point &p2) {
-	_p1 = p1;
-	_p2 = p2;
-}
-Point Vector::get_point1() {
-	return _p1;
-}
-Point Vector::get_point2() {
-	return _p2;
-}
 Vector::Vector(Point p1, Point p2) {
 	set_points(p1, p2);
 }
 Vector::~Vector() {
 }
-double Vector::x_coordinate_of_vector() {
-	return _p2.get_x() - _p1.get_x();
-}
-double Vector::y_coordinate_of_vector() {
-	return _p2.get_y() - _p1.get_y();
+
+void Vector::set_points(Point& const p1, Point& const p2) {
+	_p1 = p1;
+	_p2 = p2;
 }
 
-bool Vector::operator== (Vector& v) {
+Point Vector::get_start() const {
+	return _p1;
+}
+Point Vector::get_finish() const {
+	return _p2;
+}
+
+bool Vector::operator== (Vector& const v) {
 	Point p1, p2;
 	p1 = this->_p1;
 	p2 = this->_p2;
-	bool f = this->length() == v.length() && distance(p1,v._p1) == distance(p2,v._p2) && 
-		distance(p1,v._p1) <= distance(p1,v._p2) && distance(p2,v._p2)<= distance(p2,v._p1);
+	bool f = this->length() == v.length() && distance(p1, v._p1) == distance(p2, v._p2) &&
+		distance(p1, v._p1) <= distance(p1, v._p2) && distance(p2, v._p2) <= distance(p2, v._p1);
 	return f;
-} 
-
-bool Vector::operator!= (Vector& v) {
+}
+bool Vector::operator!= (Vector& const v) {
 	Point p1, p2;
 	p1 = this->_p1;
 	p2 = this->_p2;
@@ -39,21 +34,37 @@ bool Vector::operator!= (Vector& v) {
 	return f;
 }
 
+double Vector::x_coord() {
+	return _p2.get_x() - _p1.get_x();
+}
+double Vector::y_coord() {
+	return _p2.get_y() - _p1.get_y();
+}
 double Vector::length() {
 	return distance(_p1, _p2);
 }
-
-double Vector::scalar_product(Vector& v) {
-	return this->x_coordinate_of_vector() * v.x_coordinate_of_vector() +
-		this->y_coordinate_of_vector() * v.y_coordinate_of_vector();
+double Vector::scalar_product(Vector& const v) {
+	return this->x_coord() * v.x_coord() +
+		this->y_coord() * v.y_coord();
 }
-
-void Vector::multiplying_by_a_number(double a) {
+void Vector::multipl_by_num(double const &a) {
 	_p2.set_x(_p2.get_x() * a);
 	_p2.set_y(_p2.get_y() * a);
 }
-
 double Vector::vector_product(Vector& v) {
-	return this->x_coordinate_of_vector() * v.y_coordinate_of_vector() -
-		v.x_coordinate_of_vector() * this->y_coordinate_of_vector();
+	return this->x_coord() * v.y_coord() - v.x_coord() * this->y_coord();
+}
+void Vector::print_all_info() {
+	cout << "Given a vector with corodnites: (";
+	cout << this->x_coord() << ", " << this->y_coord() << ')' << endl;
+	cout << "Vector length: " << this->length() << endl;
+	cout << "Scalar product with vector " << endl;
+	cout << "Enter the coordinates of the vertices of the vector: ";
+	double x3, y3, x4, y4;
+	cin >> x3 >> y3 >> x4 >> y4;
+	Point p3(x3, y3);
+	Point p4(x4, y4);
+	Vector v2(p3, p4);
+	cout << endl << "Scalar product: " << this->scalar_product(v2) << endl;
+	cout << "vector product with the same vector :" << this->vector_product(v2) << endl;
 }
--- Project/Project/vector.h
+++ Project/Project/vector.h
@@ -2,31 +2,36 @@
 #include "Point.h"
 class Vector {
 public:
-	void set_points(Point& p1, Point& p2);
-	Point get_point1();
-	Point get_point2();
-
-	/*Создаем вектор по двум точкам*/
+	//констуктор по двум точкам
 	Vector(Point p1 = 0, Point p2 = 0); 
 	~Vector();
 
+	//сеттеры
+	void set_points(Point& const p1, Point& const p2);
+
+	//геттеры
+	Point get_start() const;
+	Point get_finish() const;
+
+	/*Перегрузка оператора равенства*/
+	bool operator== (Vector& const v);
+	/*Перегрузка оператора неравнества*/
+	bool operator!= (Vector& const v);
+
 	/*Нахождение х - координаты вектора*/
-	double x_coordinate_of_vector(); 
+	double x_coord(); 
 	/*Нахождение у координаты вектора*/
-	double y_coordinate_of_vector(); 
+	double y_coord(); 
 	/*Нахождение длины вектора*/
 	double length(); 
 	/*Нахождение скалярного произведения 2-х векторов*/
-	double scalar_product(Vector& v); 
+	double scalar_product(Vector& const v); 
 	/*Умножение вектора на число*/
-	void multiplying_by_a_number(double a);
+	void multipl_by_num(double const &a);
 	/*Нахождение векторного произведения 2-х векторов на плоскости*/
-	double vector_product(Vector& v); 
+	double vector_product(Vector& const v); 
+	void print_all_info();
 
-	/*Перегрузка оператора равенства*/
-	bool operator== (Vector& v);
-	/*Перегрузка оператора неравнества*/
-	bool operator!= (Vector& v);
 private:
 	Point _p1, _p2;
 };
