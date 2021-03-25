6653654ad9369e640dac1d727bdc471b122e181a
Kutuzova <postcrossing2002@mail.ru>
2021-03-25 15:56:09 +0300

    Добавлены комментарии, изменены некоторые функции, добавлена константность в класс треугольник

--- Project/Project/triangle.cpp
+++ Project/Project/triangle.cpp
@@ -3,75 +3,37 @@
 #include <iostream>
 using namespace std;
 
-void Triangle::set_point1(Point p1) {
-	_p1 = p1;
-}
-
-Point Triangle::get_point1() {
-	return _p1;
-}
-
-void Triangle::set_point2(Point p2) {
-	_p2 = p2;
-}
-
-Point Triangle::get_point2() {
-	return _p2;
-}
-
-void Triangle::set_point3(Point p3) {
-	_p3 = p3;
-}
-
-Point Triangle::get_point3() {
-	return _p3;
-}
-
 Triangle::Triangle(Point p1, Point p2, Point p3) {
 	if ((p3.get_x() - p1.get_x()) * (p2.get_y() - p1.get_y()) == (p2.get_x() - p1.get_x()) * (p3.get_y() - p1.get_y())) {
 		throw "Error: A degenerate triangle!";
 	}
 	set_point1(p1); set_point2(p2); set_point3(p3);
 }
-
 Triangle::~Triangle() {
 
 }
 
-void Triangle::print_coordinate_of_tops() {
-	_p1.print_coordinate();
-	cout << ' ';
-	_p2.print_coordinate();
-	cout << ' ';
-	_p3.print_coordinate();
-	cout << endl;
+void Triangle::set_point1(Point p1) {
+	_p1 = p1;
+}
+void Triangle::set_point2(Point p2) {
+	_p2 = p2;
+}
+void Triangle::set_point3(Point p3) {
+	_p3 = p3;
 }
 
-double Triangle::perimeter() {
-	return distance(_p1, _p2) + distance(_p2, _p3) + distance(_p1, _p3);
+Point Triangle::get_point1() const{
+	return _p1;
+}
+Point Triangle::get_point2() const {
+	return _p2;
+}
+Point Triangle::get_point3() const {
+	return _p3;
 }
 
-double Triangle::square() {
-	double a = distance(_p1, _p2);
-	double b = distance(_p2, _p3);
-	double c = distance(_p1, _p3);
-	double p = (a + b + c) / 2;
-	return sqrt(p * (p - a) * (p - b) * (p - c));
-}
-
-void Triangle::shift_right_or_left(double k) {
-	_p1.set_x(_p1.get_x() + k);
-	_p2.set_x(_p2.get_x() + k);
-	_p3.set_x(_p3.get_x() + k);
-}
-
-void Triangle::shift_up_or_down(double k) {
-	_p1.set_y(_p1.get_y() + k);
-	_p2.set_y(_p2.get_y() + k);
-	_p3.set_y(_p3.get_y() + k);
-}
-
-bool Triangle::operator== (const Triangle& t) {
+bool Triangle::operator== (const Triangle& t) const {
 	Point p1, p2, p3;
 	p1 = this->get_point1();
 	p2 = this->get_point2();
@@ -79,8 +41,7 @@ bool Triangle::operator== (const Triangle& t) {
 	bool f = distance(p1, p2) == distance(t._p1, t._p2) && distance(p2, p3) == distance(t._p2, t._p3) && distance(p1, p3) == distance(t._p1, t._p3);
 	return f;
 }
-
-bool Triangle::operator!= (const Triangle& t) {
+bool Triangle::operator!= (const Triangle& t) const {
 	Point p1, p2, p3;
 	p1 = this->get_point1();
 	p2 = this->get_point2();
@@ -89,7 +50,33 @@ bool Triangle::operator!= (const Triangle& t) {
 	return f;
 }
 
-Segment middle_line(Triangle t)
+void Triangle::print_tops()  {
+	_p1.print_coordinate();
+	cout << ' ';
+	_p2.print_coordinate();
+	cout << ' ';
+	_p3.print_coordinate();
+	cout << endl;
+}
+double Triangle::calc_perimeter() const {
+	return distance(_p1, _p2) + distance(_p2, _p3) + distance(_p1, _p3);
+}
+double Triangle::calc_square() const {
+	double a = distance(_p1, _p2);
+	double b = distance(_p2, _p3);
+	double c = distance(_p1, _p3);
+	double p = (a + b + c) / 2;
+	return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+void Triangle::shift_to_vector(double const x, double const y) {
+	_p1.set_x(_p1.get_x() + x);
+	_p2.set_x(_p2.get_x() + x);
+	_p3.set_x(_p3.get_x() + x);
+	_p1.set_y(_p1.get_y() + y);
+	_p2.set_y(_p2.get_y() + y);
+	_p3.set_y(_p3.get_y() + y);
+}
+Segment middle_line(Triangle const &t)
 {
 	int k;
 	Point p1 = t.get_point1(), p2 = t.get_point2(), p3 = t.get_point3();
@@ -118,4 +105,12 @@ Segment middle_line(Triangle t)
 		}
 	}
 	return Segment();
+}
+void Triangle::print_all_info() {
+	cout << "Given a triangle with vertices with corodnites: ";
+	this->print_tops();
+	double per = this->calc_perimeter();
+	double sq = this->calc_square();
+	cout << "Perimeter: ";
+	cout << per << " Square: " << sq << endl;
 }
--- Project/Project/triangle.h
+++ Project/Project/triangle.h
@@ -3,39 +3,39 @@
 #include "segment.h"
 class Triangle {
 public:
-	void set_point1(Point p1);
-	Point get_point1();
-
-	void set_point2(Point p2);
-	Point get_point2();
-
-	void set_point3(Point p3);
-	Point get_point3();
-
-	/*Задание треугольника по трем точкам*/
+	//Конструктор по 3 точкам
 	Triangle(Point p1 = 0, Point p2 = 0, Point p3 = 0);
 	~Triangle();
 
-	/*Печать координат вершин треугольника*/
-	void print_coordinate_of_tops();//печатает координаты вершин треугольника
+	//сеттеры
+	void set_point1(Point p1);
+	void set_point2(Point p2);
+	void set_point3(Point p3);
 
-	/*Периметр треугольника*/
-	double perimeter();
-	/*Площадь треугольника*/
-	double square();
-
-	/*При k>0 сдвиг вправо, иначе влево*/
-	void shift_right_or_left(double k);
-	/*При k>0 сдвиг вверх, иначе вниз*/
-	void shift_up_or_down(double k);
+	//геттеры
+	Point get_point1() const;
+	Point get_point2() const;
+	Point get_point3() const;
 
 	/*Перегрузка оператора равенства*/
-	bool operator== (const Triangle& t);
+	bool operator== (const Triangle& t) const;
 	/*Перегрузка оператора неравнества*/
-	bool operator!= (const Triangle& t);
+	bool operator!= (const Triangle& t) const;
 
-	/*Средняя линия треугольника*/
-	friend Segment middle_line(Triangle t);
+	//вспомогательные функции
+	//Печать координат вершин треугольника
+	void print_tops();//печатает координаты вершин треугольника
+	//Периметр треугольника
+	double calc_perimeter() const;
+	//Площадь треугольника
+	double calc_square() const;
+	//сдвиг треугольника на вектор
+	void shift_to_vector(double const x, double const y);
+	//Средняя линия треугольника
+	friend Segment middle_line(Triangle const &t);
+	//вывод всей информации о треугольнике
+	void print_all_info();
+
 private:
 	Point _p1, _p2, _p3;
 };
