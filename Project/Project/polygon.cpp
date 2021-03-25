5c5ddb6799c9c2da704937700768e1061678d21a
MakarSi <MakarSi>
2021-03-25 20:55:44 +0300

    privacy changed, indexes in funcs changed

--- Project/Project/polygon.cpp
+++ Project/Project/polygon.cpp
@@ -5,8 +5,8 @@ namespace polygon {
 	Polygon::Polygon(vector<Point> vertexes)
 	{
 		set_vertexes(vertexes);
-		set_area();
-		set_perimeter();
+		init_area();
+		init_perimeter();
 	}
 
 	Point Polygon::get_vertex(int i)const {
@@ -27,7 +27,7 @@ namespace polygon {
 	double Polygon::get_perimeter()const {
 		return _perimeter;
 	}
-
+		
 	void Polygon::set_vertexes(vector<Point> vertexes) {
 		if (vertexes.size() < 3 && vertexes.size() != 0)
 			throw invalid_argument("Not enough vertexes");
@@ -52,47 +52,42 @@ namespace polygon {
 		}
 	}
 
-	void Polygon::set_perimeter() {
-		if (get_dim() == 0) {
+	void Polygon::init_perimeter() {
+		size_t dim = get_dim();
+		if (dim == 0) {
 			_perimeter = 0;
 			return;
 		}
 		double res = 0;
-		for (int i = 0; i < get_dim() - 1; i++)
-			res += distance(get_vertex(i), get_vertex(i + 1));
-		res += distance(get_vertex(0), get_vertex(get_dim() - 1));
+		for (int i = 0; i < dim; i++)
+			res += distance(get_vertex(i % dim), get_vertex((i + 1) % dim));
 		_perimeter = res;
 	}
 
-	void Polygon::set_area() {
-		if (get_dim() == 0) {
+	void Polygon::init_area() {
+		int dim = get_dim();
+		if (dim == 0) {
 			_area = 0;
 			return;
 		}
 		//воспользуемся формулой площади Гаусса
 		double res = 0;
-		if (get_dim() < 3) {
-			_area = 0;
-			return;
+		for (int i = 0; i < dim; i++) {
+			res += get_vertex(i % dim).get_x() * get_vertex((i + 1) % dim).get_y();
+			res -= get_vertex((i + 1) % dim).get_x() * get_vertex(i % dim).get_y();
 		}
-		for (int i = 0; i < get_dim() - 1; i++) {
-			res += get_vertex(i).get_x() * get_vertex(i + 1).get_y();
-			res -= get_vertex(i + 1).get_x() * get_vertex(i).get_y();
-		}
-		res += get_vertex(get_dim() - 1).get_x() * get_vertex(0).get_y();
-		res -= get_vertex(0).get_x() * get_vertex(get_dim() - 1).get_y();
 		res = abs(res) / 2;
 		_area = res;
 	}
 
-	void Polygon::print_coords(ostream& out) {
+	void Polygon::print_coords(ostream& out)const {
 		int size = get_dim();
 		for (int i = 0; i < size - 1; i++)
 			out << get_vertex(i) << ", ";
 		out << get_vertex(size - 1) << endl;
 	}
 
-	void Polygon::print_all_info(ostream& out) {
+	void Polygon::print_all_info(ostream& out)const {
 		out << "Coords of all vertexes: ";
 		print_coords(out);
 		out << "Area: " << get_area() << endl;
--- Project/Project/polygon.h
+++ Project/Project/polygon.h
@@ -7,15 +7,23 @@ using namespace std;
 
 namespace polygon {
 	class Polygon
-	{
+	{	
+	private:
+		//многограник храним как упорядоченный набор его вершин
+		double _area, _perimeter;
+		vector<Point> _vertexes;
+
+		//вычисление периметра 
+		void init_perimeter();
+		//вычисление площади
+		void init_area();
+
 	public:
 		Polygon(vector<Point> vertexes = {});
+
 		//установление вершин, самопересекающийся многоугольник считаем невалидным
 		void set_vertexes(vector<Point>);
-		//вычисление периметра 
-		void set_perimeter();
-		//вычисление площади
-		void set_area();
+
 		size_t get_dim()const;
 		Point get_vertex(int)const;
 		double get_area()const;
@@ -24,13 +32,10 @@ namespace polygon {
 		friend istream& operator>> (istream&, Polygon&);
 		/*bool operator== (const Polygon&);
 		bool operator!= (const Polygon&);*/
+
 		//вывод координат всех вершин
-		void print_coords(ostream&);
+		void print_coords(ostream&)const;
 		//вывод всей информации о четырехугольнике
-		void print_all_info(ostream&);
-	private:
-		//многограник храним как упорядоченный набор его вершин
-		double _area, _perimeter;
-		vector<Point> _vertexes;
+		void print_all_info(ostream&)const;
 	};
 }
