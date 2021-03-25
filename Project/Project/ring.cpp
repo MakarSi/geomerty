0db310aa8fce35afdd03f46500b0fb8e76b8f00c
PetrovaAnastasiia <petrovanastya_01@mail.ru>
2021-03-25 20:09:50 +0300

    Added constants and comments

--- Project/Project/ring.cpp
+++ Project/Project/ring.cpp
@@ -1,28 +1,7 @@
 #define _USE_MATH_DEFINES 
 #include "ring.h"
-#include <iostream>
-#include <math.h>
 
 using namespace std;
-
-void Ring::set_center(Point center) {
-	_center = center;
-}
-
-Point Ring::get_center() {
-	return _center;
-}
-
-void Ring::set_rad(double rad) {
-	if (rad <= 0) {
-		_rad = 1;     //В случае неправильного ввода - присваивается радиусу 1
-	}
-	else _rad = rad;
-}
-
-double Ring::get_rad() {
-	return _rad;
-}
 
 Ring::Ring(Point center, double _rad) {
 	set_center(center), set_rad(_rad);
@@ -32,13 +11,34 @@ Ring::~Ring() {
 
 }
 
-double Ring::square(Ring c1) {
+void Ring::set_center(Point center) {
+	_center = center;
+}
+
+void Ring::set_rad(double rad) {
+	if (rad <= 0) {
+		cout << "Error assigning a negative or zero value to the radius " << endl;
+
+		_rad = 1;     //В случае неправильного ввода - присваивается радиусу 1
+	}
+	else _rad = rad;
+}
+
+Point Ring::get_center() const {
+	return _center;
+}
+
+double Ring::get_rad() const {
+	return _rad;
+}
+
+double Ring::square(Ring const& c1) {
 	double r = c1.get_rad();
 	double l = M_PI * r * r;
 	return l;
 }
 
-double Ring::sector(Ring c1, double cor) {
+double Ring::sector(Ring const& c1, double cor) {
 	double r = c1.get_rad();
 	if (cor > 360) {
 		while (cor > 360)
--- Project/Project/ring.h
+++ Project/Project/ring.h
@@ -1,23 +1,27 @@
 #pragma once
-#include "Point.h"
+#include "point.h"
 #include <iostream>
 #include <math.h>
 
 class Ring {
 public:
+	/*Конструктор по точке центра и радиусу*/
 	Ring(Point center = 0, double _rad = 1);
 	~Ring();
 
+	/*Сеттеры*/
 	void set_center(Point center);
-	Point get_center();
-
-	double get_rad();
 	void set_rad(double _rad);
 
-	/*Нахождение площади круга*/
-	double square(Ring c1);
-	/*Нахождение площади сегмента*/
-	double sector(Ring c1, double cor);
+	/*Геттеры*/
+	Point get_center() const;
+	double get_rad() const;
+
+	/*Вспомогательные функции*/
+	/*Площадь круга*/
+	double square(Ring const& c1);
+	/*Площадь кругового сектора - в функции передается фигура и угол в градусах*/
+	double sector(Ring const& c1, double cor);
 private:
 	Point _center;
 	double _rad;
