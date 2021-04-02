b97d465c42c78d7ce76537dbce52663fee4f78bb
Kutuzova <postcrossing2002@mail.ru>
2021-04-02 22:59:39 +0300

    исправлен класс треугольник

--- Project/Project/triangle.cpp
+++ Project/Project/triangle.cpp
@@ -3,83 +3,29 @@
 #include <iostream>
 using namespace std;
 
-Triangle::Triangle(Point p1, Point p2, Point p3) {
-	if ((p3.get_x() - p1.get_x()) * (p2.get_y() - p1.get_y()) == (p2.get_x() - p1.get_x()) * (p3.get_y() - p1.get_y())) {
-		throw "Error: A degenerate triangle!";
-	}
-	set_point1(p1); set_point2(p2); set_point3(p3);
+Triangle::Triangle(vector<Point> vertexes)
+{
+	set_vertexes(vertexes);
+	init_area();
+	init_perimeter();
 }
 Triangle::~Triangle() {
 
 }
 
-void Triangle::set_point1(Point p1) {
-	_p1 = p1;
-}
-void Triangle::set_point2(Point p2) {
-	_p2 = p2;
-}
-void Triangle::set_point3(Point p3) {
-	_p3 = p3;
-}
-
-Point Triangle::get_point1() const{
-	return _p1;
-}
-Point Triangle::get_point2() const {
-	return _p2;
-}
-Point Triangle::get_point3() const {
-	return _p3;
-}
-
 bool Triangle::operator== (const Triangle& t) const {
-	Point p1, p2, p3;
-	p1 = this->get_point1();
-	p2 = this->get_point2();
-	p3 = this->get_point3();
-	bool f = distance(p1, p2) == distance(t._p1, t._p2) && distance(p2, p3) == distance(t._p2, t._p3) && distance(p1, p3) == distance(t._p1, t._p3);
+	bool f = distance(_p1, _p2) == distance(t._p1, t._p2) && distance(_p2, _p3) == distance(t._p2, t._p3) && distance(_p1, _p3) == distance(t._p1, t._p3);
 	return f;
 }
 bool Triangle::operator!= (const Triangle& t) const {
-	Point p1, p2, p3;
-	p1 = this->get_point1();
-	p2 = this->get_point2();
-	p3 = this->get_point3();
-	bool f = distance(p1, p2) != distance(t._p1, t._p2) || distance(p2, p3) != distance(t._p2, t._p3) || distance(p1, p3) != distance(t._p1, t._p3);
+	bool f = distance(_p1, _p2) != distance(t._p1, t._p2) || distance(_p2, _p3) != distance(t._p2, t._p3) || distance(_p1, _p3) != distance(t._p1, t._p3);
 	return f;
 }
 
-void Triangle::print_tops()  {
-	_p1.print_coordinate();
-	cout << ' ';
-	_p2.print_coordinate();
-	cout << ' ';
-	_p3.print_coordinate();
-	cout << endl;
-}
-double Triangle::calc_perimeter() const {
-	return distance(_p1, _p2) + distance(_p2, _p3) + distance(_p1, _p3);
-}
-double Triangle::calc_square() const {
-	double a = distance(_p1, _p2);
-	double b = distance(_p2, _p3);
-	double c = distance(_p1, _p3);
-	double p = (a + b + c) / 2;
-	return sqrt(p * (p - a) * (p - b) * (p - c));
-}
-void Triangle::shift_to_vector(double const x, double const y) {
-	_p1.set_x(_p1.get_x() + x);
-	_p2.set_x(_p2.get_x() + x);
-	_p3.set_x(_p3.get_x() + x);
-	_p1.set_y(_p1.get_y() + y);
-	_p2.set_y(_p2.get_y() + y);
-	_p3.set_y(_p3.get_y() + y);
-}
 Segment middle_line(Triangle const& t)
 {
 	int k;
-	Point p1 = t.get_point1(), p2 = t.get_point2(), p3 = t.get_point3();
+	Point p1 = t.get_vertex(0), p2 = t.get_vertex(1), p3 = t.get_vertex(2);
 	Point k1, k2;
 	Segment s1 = Segment(p1, p2), s2 = Segment(p2, p3), s3 = Segment(p3, p1);
 	cout << "Input the side you want the middle line be parallel to\n";
@@ -107,12 +53,4 @@ Segment middle_line(Triangle const& t)
 		return Segment(k1, k2);
 	}
 	return Segment();
-}
-void Triangle::print_all_info() {
-	cout << "Given a triangle with vertices with corodnites: ";
-	this->print_tops();
-	double per = this->calc_perimeter();
-	double sq = this->calc_square();
-	cout << "Perimeter: ";
-	cout << per << " Square: " << sq << endl;
 }
--- Project/Project/triangle.h
+++ Project/Project/triangle.h
@@ -1,21 +1,12 @@
 #pragma once
 #include "point.h"
 #include "segment.h"
-class Triangle: public Object {
+#include "polygon.h"
+class Triangle: public polygon::Polygon {
 public:
 	//Конструктор по 3 точкам
-	Triangle(Point p1 = 0, Point p2 = 0, Point p3 = 0);
+	Triangle(vector<Point> vertexes = {});
 	~Triangle();
-
-	//сеттеры
-	void set_point1(Point p1);
-	void set_point2(Point p2);
-	void set_point3(Point p3);
-
-	//геттеры
-	Point get_point1() const;
-	Point get_point2() const;
-	Point get_point3() const;
 
 	/*Перегрузка оператора равенства*/
 	bool operator== (const Triangle& t) const;
@@ -23,18 +14,8 @@ public:
 	bool operator!= (const Triangle& t) const;
 
 	//вспомогательные функции
-	//Печать координат вершин треугольника
-	void print_tops();//печатает координаты вершин треугольника
-	//Периметр треугольника
-	double calc_perimeter() const;
-	//Площадь треугольника
-	double calc_square() const;
-	//сдвиг треугольника на вектор
-	void shift_to_vector(double const x, double const y);
 	//Средняя линия треугольника
 	friend Segment middle_line(Triangle const &t);
-	//вывод всей информации о треугольнике
-	void print_all_info();
 
 private:
 	Point _p1, _p2, _p3;
